e07713784fe1ccd9519d3aed901f54b49e63cd21
Victor <victoorqrz@gmail.com>
2019-09-01 17:00:43 -0300

    implementacao de ProductSale

--- src/ufs/br/poostore/models/ProductSale.java
+++ src/ufs/br/poostore/models/ProductSale.java
@@ -1,6 +1,8 @@
 package ufs.br.poostore.models;
 
-public class ProductSale extends Product {
+import java.io.Serializable;
+
+public class ProductSale extends Product implements Serializable, Equals<ProductSale>{
     
     private int soldQuantity;
     private float total;
@@ -26,5 +28,15 @@ public class ProductSale extends Product {
 
     public void setTotal(float total) {
         this.total = total;
+    }
+    
+    @Override
+    public String toString(){
+        return "Código " + id + "- Nome: " + name;
+    }
+    
+    @Override
+    public boolean isRegistered(ProductSale obj){
+        return this.name.equalsIgnoreCase(obj.getName());
     }
 }
